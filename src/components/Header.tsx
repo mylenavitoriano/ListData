89f2c302d087c1b3f0888672ce4d33aabc906f28
Mylena <mylenabarbosa08@gmail.com>
2025-03-02 18:09:30 -0300

    feat: changed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import Image from "next/image";
 import ThemeSwitch from "./ThemeSwitch";
+import LogoLight from "../../public/logo-light.png";
+import LogoDark from "../../public/logo-dark.png";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const { resolvedTheme } = useTheme();
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      setTheme(resolvedTheme as "light" | "dark");
+    }
+  }, [resolvedTheme]);
+
   return (
     <header className="py-4">
-      <div className="flex items-center justify-between ">
-        <p className="text-lg font-semibold">List.Data</p>
+      <div className="flex items-center justify-between">
+        <Image
+          src={theme === "light" ? LogoLight : LogoDark}
+          alt="Logo List.Data"
+          priority
+        />
         <ThemeSwitch />
       </div>
       <hr className="border-t border-secondary my-4" />
